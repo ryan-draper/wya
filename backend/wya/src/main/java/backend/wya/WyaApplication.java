a3c731b0336f20a6693f8c0d27b170f9bdb7a8b1
Pete Sirithanachai <70217951+petepasan@users.noreply.github.com>
2022-09-24 16:20:37 -0500

    spring boot

---
+++ backend/wya/src/main/java/backend/wya/WyaApplication.java
@@ -0,0 +1,13 @@
+package backend.wya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WyaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WyaApplication.class, args);
+	}
+
+}
