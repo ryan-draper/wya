620adf9c70d1cd49829850a0e5bcd7db9d0944b9
ryan-draper <draperryanm@gmail.com>
2022-09-24 22:27:31 -0500

    Add image upload to extension frontend.

--- extension/popup.js
+++ extension/popup.js
@@ -1,52 +1,13 @@
-// // Initialize button with user's preferred color
-// let changeColor = document.getElementById("changeColor");
-
-// chrome.storage.sync.get("color", ({ color }) => {
-//   changeColor.style.backgroundColor = color;
-// });
-
-// // When the button is clicked, inject setPageBackgroundColor into current page
-// changeColor.addEventListener("click", async () => {
-//     let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-  
-//     chrome.scripting.executeScript({
-//       target: { tabId: tab.id },
-//       func: setPageBackgroundColor,
-//     });
-//   });
-  
-//   // The body of this function will be executed as a content script inside the
-//   // current page
-//   function setPageBackgroundColor() {
-//     chrome.storage.sync.get("color", ({ color }) => {
-//       document.body.style.backgroundColor = color;
-//     });
-//   }
-
-// function httpGetAsync(theUrl, callback) {
-//     console.log('yo');
-//     var xmlHttp = new XMLHttpRequest();
-//     xmlHttp.onreadystatechange = function() { 
-//         if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
-//             callback(xmlHttp.responseText);
-//     }
-//     xmlHttp.open("GET", theUrl, true); // true for asynchronous 
-//     xmlHttp.send(null);
-// }
-
-function httpGet(theUrl)
-{
-    var xmlHttp = new XMLHttpRequest();
-    xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
-    xmlHttp.send( null );
-    //console.log('2');
-    //console.log(xmlHttp.responseText);
-    document.write(xmlHttp.responseText);
-    //return xmlHttp.responseText;
+function httpGet(theUrl) {
+    // var xmlHttp = new XMLHttpRequest();
+    // xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
+    // xmlHttp.send( null );
+    // document.getElementById("upload").innerHTML = xmlHttp.responseText;
+    // document.write(xmlHttp.responseText);
 }
 
 window.onload = function() {
-    httpGet('http://localhost:8080/api/v1/uploadimage');
+    // httpGet('http://localhost:8080/api/v1/uploadimage');
     //console.log('1');
 };
 
